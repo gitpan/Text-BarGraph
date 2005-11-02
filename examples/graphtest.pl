b5cd12e3b9b45b1cc77aed62f483f5844aa26957
Kirk Baucom <kbaucom@schizoid.com>
2005-11-02 18:50:48 +0000

    Import of KBAUCOM/Text-BarGraph-1.0 from CPAN.
    gitpan-cpan-distribution: Text-BarGraph
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         KBAUCOM/Text-BarGraph-1.0.tar.gz
    gitpan-cpan-author:       KBAUCOM
    gitpan-cpan-maturity:     released

--- examples/graphtest.pl
+++ examples/graphtest.pl
@@ -1,7 +1,6 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl -w
 #
 # Example of the Text::Bargraph module
-# Try commenting and uncommenting the options to see how they affect the output
 #
 # Kirk Baucom <kbaucom@schizoid.com>
 
@@ -12,40 +11,41 @@ $g = Text::BarGraph->new();
 #### OPTIONS ####
 
 # for the options below, if an option is a toggle, 0 = off, 1 = on
-# all of the options have defaults set at the beginning of the module
+# all of the options have defaults
 
 # whether or not to print the numerical magnitude of the bar
-$g->{'num'} = 1;    # default: 0 (off)
+$g->num(1);    # default: 0 (off)
 
 # what value to set the far right of the screen to
-$g->{'max_data'} = 100;   # default: automatically determined from data
+$g->max_data(100);   # default: automatically determined from data
 
 # what character to use when printing the graphs
-$g->{'dot'} = "#";    # default: '.'
+$g->dot('#');    # default: '.'
 
 # what value to set the far left of the screen to
-$g->{'zero'} = 10;   # default: 0
+$g->zero(10);   # default: 0
 
 # whether or not to automatically determine the value of the far left side
 # of the screen. if this is set, the value of 'zero' is ignored.
-$g->{'autozero'} = 1;   # default: 0 (off)
+#$g->autozero(1);   # default: 0 (off)
 
 # whether or not to automatically determine the size of your screen. this
 # requires the module Term::Readkey. if this is off, your screen is assumed
 # to be 80 columns
-$g->{'autosize'} = 1;    # default: 0 (off)
+$g->autosize(1);    # default: 1 (on)
 
-# number of lines on your display
-$g->{'xsize'} = 80;      # default: 80
+# number of columns on your display
+# this is ignored if autosize is set
+$g->columns(80);      # default: 80
 
 # whether to sort the data by keys ("key") or values ("data"). 
-$g->{'sort'} = "data";  # default: "key"
+$g->sortvalue("data");  # default: "key"
 
 # whether to sort keys numerically or stringily (stringmatogically?)
-$g->{'sorttype'} = "numeric";  # default: "string"
+$g->sorttype("numeric");  # default: "string"
 
 # add color to the graph, denoting the size of the bars
-$g->{'color'} = 1;      # default: 0 (off)
+$g->enable_color(1);      # default: 0 (off)
 
 # a small graph of some random numbers  
 
